e4159f4b0c02b60803e15fa19be005207625d6b9
Lore Oregi <loreoregi@gmail.com>
2025-12-12 10:14:37 +0100

    Initial commit lab2

---
+++ lab2/utils.py
@@ -0,0 +1,132 @@
+import numpy as np
+from math import ceil
+import matplotlib.pyplot as plt
+from scipy.ndimage import map_coordinates
+import random
+import plotly.graph_objects as go
+
+def plot_img(img, do_not_use=[0]):
+    plt.figure(do_not_use[0])
+    do_not_use[0] += 1
+    plt.imshow(img)
+
+
+def get_transformed_pixels_coords(I, H, shift=None):
+    ys, xs = np.indices(I.shape[:2]).astype("float64")
+    if shift is not None:
+        ys += shift[1]
+        xs += shift[0]
+    ones = np.ones(I.shape[:2])
+    coords = np.stack((xs, ys, ones), axis=2)
+    coords_H = (H @ coords.reshape(-1, 3).T).T
+    coords_H /= coords_H[:, 2, np.newaxis]
+    cart_H = coords_H[:, :2]
+    
+    return cart_H.reshape((*I.shape[:2], 2))
+
+def apply_H_fixed_image_size(I, H, corners):
+
+    # ToDo: complete this code .......
+    return
+    
+
+def Normalise_last_coord(x):
+    xn = x  / x[2,:]
+    
+    return xn
+
+def DLT_homography(points1, points2):
+    
+    # ToDo: complete this code .......
+
+    return H
+
+def Inliers(H, points1, points2, th):
+    
+    # Check that H is invertible
+    if abs(math.log(np.linalg.cond(H))) > 15:
+        idx = np.empty(1)
+        return idx
+    
+    
+    # ToDo: complete this code .......
+    
+    return
+
+def Ransac_DLT_homography(points1, points2, th, max_it):
+    
+    Ncoords, Npts = points1.shape
+    
+    it = 0
+    best_inliers = np.empty(1)
+    
+    while it < max_it:
+        indices = random.sample(range(1, Npts), 4)
+        H = DLT_homography(points1[:,indices], points2[:,indices])
+        inliers = Inliers(H, points1, points2, th)
+        
+        # test if it is the best model so far
+        if inliers.shape[0] > best_inliers.shape[0]:
+            best_inliers = inliers
+        
+        # update estimate of iterations (the number of trials) to ensure we pick, with probability p,
+        # an initial data set with no outliers
+        fracinliers = inliers.shape[0]/Npts
+        pNoOutliers = 1 -  fracinliers**4
+        eps = sys.float_info.epsilon
+        pNoOutliers = max(eps, pNoOutliers)   # avoid division by -Inf
+        pNoOutliers = min(1-eps, pNoOutliers) # avoid division by 0
+        p = 0.99
+        max_it = math.log(1-p)/math.log(pNoOutliers)
+        
+        it += 1
+    
+    # compute H from all the inliers
+    H = DLT_homography(points1[:,best_inliers], points2[:,best_inliers])
+    inliers = best_inliers
+    
+    return H, inliers
+
+
+
+def optical_center(P):
+    U, d, Vt = np.linalg.svd(P)
+    o = Vt[-1, :3] / Vt[-1, -1]
+    return o
+
+def view_direction(P, x):
+    # Vector pointing to the viewing direction of a pixel
+    # We solve x = P v with v(3) = 0
+    v = np.linalg.inv(P[:,:3]) @ np.array([x[0], x[1], 1])
+    return v
+
+def plot_camera(P, w, h, fig, legend):
+    
+    o = optical_center(P)
+    scale = 200
+    p1 = o + view_direction(P, [0, 0]) * scale
+    p2 = o + view_direction(P, [w, 0]) * scale
+    p3 = o + view_direction(P, [w, h]) * scale
+    p4 = o + view_direction(P, [0, h]) * scale
+    
+    x = np.array([p1[0], p2[0], o[0], p3[0], p2[0], p3[0], p4[0], p1[0], o[0], p4[0], o[0], (p1[0]+p2[0])/2])
+    y = np.array([p1[1], p2[1], o[1], p3[1], p2[1], p3[1], p4[1], p1[1], o[1], p4[1], o[1], (p1[1]+p2[1])/2])
+    z = np.array([p1[2], p2[2], o[2], p3[2], p2[2], p3[2], p4[2], p1[2], o[2], p4[2], o[2], (p1[2]+p2[2])/2])
+    
+    fig.add_trace(go.Scatter3d(x=x, y=z, z=-y, mode='lines',name=legend))
+    
+    return
+
+def plot_image_origin(w, h, fig, legend):
+    p1 = np.array([0, 0, 0])
+    p2 = np.array([w, 0, 0])
+    p3 = np.array([w, h, 0])
+    p4 = np.array([0, h, 0])
+    
+    x = np.array([p1[0], p2[0], p3[0], p4[0], p1[0]])
+    y = np.array([p1[1], p2[1], p3[1], p4[1], p1[1]])
+    z = np.array([p1[2], p2[2], p3[2], p4[2], p1[2]])
+    
+    fig.add_trace(go.Scatter3d(x=x, y=z, z=-y, mode='lines',name=legend))
+    
+    return
