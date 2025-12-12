4f529522118f90776eb3493ddb34449dd677131a
Lore Oregi <loreoregi@gmail.com>
2025-12-12 12:07:45 +0100

    1. Homography estimation with the DLT algorithm DONE

--- lab2/utils.py
+++ lab2/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
-from math import ceil
+import math
+import sys
 import matplotlib.pyplot as plt
 from scipy.ndimage import map_coordinates
 import random
@@ -35,9 +36,75 @@ def Normalise_last_coord(x):
     
     return xn
 
+def Normalize_points(points):
+    """
+    Normalization: centroid -> (0,0) and mean distance -> sqrt(2)
+    points: 3xN homogeneous
+    returns: points_n (3xN), T (3x3) with points_n = T @ points
+    """
+    p = Normalise_last_coord(points)
+    x, y = p[0, :], p[1, :]
+
+    cx, cy = np.mean(x), np.mean(y)
+    x_shift = x - cx
+    y_shift = y - cy
+
+    mean_dist = np.mean(np.sqrt(x_shift**2 + y_shift**2)) + 1e-8
+    s = np.sqrt(2) / mean_dist
+
+    T = np.array([
+        [s, 0, -s*cx],
+        [0, s, -s*cy],
+        [0, 0,   1  ]
+    ], dtype=float)
+
+    points_n = T @ p
+    return points_n, T
+
+
 def DLT_homography(points1, points2):
+    """
+    Normalized DLT for homography estimation
+    points1: 3xN (homogeneous) in image 1
+    points2: 3xN (homogeneous) in image 2
+    returns H such that points2 ~ H * points1
+    """
+    assert points1.shape[0] == 3 and points2.shape[0] == 3 
+    assert points1.shape[1] >= 4 and points2.shape[1] == points1.shape[1] # Minimum 4 correspondences needed
+
+    # Normalize points
+    points1, T1 = Normalize_points(points1)
+    points2, T2 = Normalize_points(points2)
     
-    # ToDo: complete this code .......
+    N = points1.shape[1]
+    A = []
+
+    # For each correspondence, compute Ai
+    for i in range(N):
+        x, y, w  = points1[:, i]
+        xp, yp, wp = points2[:, i] 
+        
+        # Two equations per correspondence
+        A.append([0, 0, 0, -wp*x, -wp*y, -wp*w,  yp*x,  yp*y,  yp*w])
+        A.append([wp*x, wp*y, wp*w, 0, 0, 0, -xp*x, -xp*y, -xp*w])
+
+    # Assemble the N matrices Ai to form the 2N x 9 matrix A
+    A = np.asarray(A)
+    assert A.shape == (2*N, 9)
+
+    # Compute the SVD of A
+    _, _, Vt = np.linalg.svd(A)
+    h = Vt[-1, :] # Last row of Vt = last column of V, corresponds to smallest singular value 
+    Hn = h.reshape(3, 3) # Reshape h to form Hn (normalized homography)
+
+    # Denormalize the homography
+    H = np.linalg.inv(T2) @ Hn @ T1
+
+    # Normalize scale (homography is defined up to scale; we fix it by setting H[2,2] = 1 when possible)
+    if abs(H[2,2]) > 1e-12:
+        H = H / H[2,2]
+    else:
+        H = H / (np.linalg.norm(H) + 1e-12)
 
     return H
 
@@ -48,10 +115,29 @@ def Inliers(H, points1, points2, th):
         idx = np.empty(1)
         return idx
     
+    # Forward projection 
+    fwd_proj = H @ points1
+    fwd_proj = Normalise_last_coord(fwd_proj)
+
+    # Backward projection
+    H_inv = np.linalg.inv(H)
+    bwd_proj = H_inv @ points2
+    bwd_proj = Normalise_last_coord(bwd_proj)
+
+    # Normalize points
+    points1 = Normalise_last_coord(points1)
+    points2 = Normalise_last_coord(points2)
+
+    # Compute symmetric transfer error
+    error1 = np.linalg.norm(points2[:2, :] - fwd_proj[:2, :], axis=0)**2
+    error2 = np.linalg.norm(points1[:2, :] - bwd_proj[:2, :], axis=0)**2
+    error = np.sqrt(error1 + error2)
     
-    # ToDo: complete this code .......
-    
-    return
+    # Inliers are those with error below threshold th
+    idx = np.where(error < th)[0]
+
+    return idx
+
 
 def Ransac_DLT_homography(points1, points2, th, max_it):
     
